7ee58bffa11bdbfa7e650d2c31e687359562ad20
jh <jaehoon0822@gmail.com>
2021-06-22 20:11:12 +0900

    server distribute2 chatper

--- src/server.js
+++ src/server.js
@@ -3,11 +3,13 @@ const mongoose = require("mongoose");
 const {
 	userRouter, blogRouter
 } = require("./routes");
-const { generateFakeData } = require("../faker2")
+// const { generateFakeData } = require("../faker2")
 
 const app = express();
-const port = 3000;
-const MONGO_URI = 'mongodb+srv://admin:9KFH9ObdjVJnbQUF@mongodbtutorial.uksk8.mongodb.net/BlogService?retryWrites=true&w=majority'
+const { PORT } = process.env;
+const { MONGO_URI } = process.env
+
+if (!MONGO_URI) throw new Error("MONGO_URI is not exeist");
 
 const server = async () => {
 	try {
@@ -24,11 +26,11 @@ const server = async () => {
 		app.use('/user', userRouter);
 		app.use('/blog', blogRouter);
 
-		app.listen(port, async () => {
-			console.log(`Example app listening at http://localhost:${port}`);
-			console.time("execution time: ");
-			await generateFakeData(10, 2, 10);
-			console.timeEnd("execution time: ");
+		app.listen(PORT, async () => {
+			console.log(`Example app listening at http://localhost:${PORT}`);
+			// console.time("execution time: ");
+			// await generateFakeData(10, 2, 10);
+			// console.timeEnd("execution time: ");
 		});
 
 	} catch (err) {
